695e63bb06c249257fc407666cd699b09e195d34
RegithubDev <regithub10@gmail.com>
2024-05-04 16:56:13 +0530

    Commit

--- src/main/java/com/resustainability/reisp/controller/Schedular.java
+++ src/main/java/com/resustainability/reisp/controller/Schedular.java
@@ -72,16 +72,15 @@ public class Schedular {
  		if(is_cron_jobs_enabled || is_cron_jobs_enabled_in_qa) {
  			List<DashBoardWeighBridge> pData = service.projectsIWMList("IWM");
  		    LocalDate yesterday = LocalDate.now().minusDays(1);
- 	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+ 	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
  	        String yesterdaysDate = yesterday.format(formatter);
  			pData.forEach(plist-> {
  				
  				 String url = "http://10.100.1.7:8000/sap/opu/odata/sap/Z_IDWM_WEIGHT_CDS/Z_IDWM_WEIGHT?$format=json"
- 				 		+ "&$filter=aedat_changedDate%20eq%20%27"
+ 				 		+ "&$filter=20CreationDate%20eq%20%27"
  			    		 
 		     		+ yesterdaysDate
-		     		
-		     		+ "%27%20or%20erdat_creationDate%20eq%20%27"
+		     		+ "%27%20or%20CreationDate%20ge%20datetime%27"
 		     		
 		     		+ yesterdaysDate
 		     		
@@ -167,16 +166,193 @@ public class Schedular {
 		 HashMap<String, String> data1 = new HashMap<String, String>();
 		 ObjectMapper objectMapper = new ObjectMapper();
  		if(is_cron_jobs_enabled || is_cron_jobs_enabled_in_qa) {
- 			List<DashBoardWeighBridge> pData = service.projectsIWMList("IWM");
+ 			//List<DashBoardWeighBridge> pData = service.projectsIWMList("IWM");
  		    LocalDate yesterday = LocalDate.now().minusDays(1);
- 	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+ 	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
  	        System.out.println("wfw");
  	        String yesterdaysDate = yesterday.format(formatter);
- 			pData.forEach(plist-> {
+ 			//pData.forEach(plist-> {
+ 	
+
+		     String url = "http://10.100.1.7:8000/sap/opu/odata/sap/Z_IDWM_WEIGHT_CDS/Z_IDWM_WEIGHT?$format=json&$filter=Werks_plant%20eq%20%273626%27%20and%20"
+		     		+ "CreationDate%20ge%20datetime%27"
+		     		+ yesterdaysDate
+		     		+ "T24:00:00%27%20";
+	            String username = "22011982";
+	            String password = "Ramky@567";
+
+	            CredentialsProvider provider = new BasicCredentialsProvider();
+	            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
+	            provider.setCredentials(AuthScope.ANY, credentials);
+
+	            try (CloseableHttpClient httpClient = HttpClients.custom()
+	                    .setDefaultCredentialsProvider(provider)
+	                    .build()) {
+	                HttpGet request = new HttpGet(url);
+
+	                HttpResponse response = httpClient.execute(request);
+
+	                if (response.getStatusLine().getStatusCode() == 200) {
+	                    HttpEntity entity = response.getEntity();
+	                    String responseBody = EntityUtils.toString(entity);
+	                    // Parse the JSON response
+	                    JSONObject jsonResponse = new JSONObject(responseBody);
+	                    JSONObject dObject = jsonResponse.getJSONObject("d");
+	                    JSONArray resultsArray = dObject.getJSONArray("results");
+
+
+	                    // Process the data and set it in a list
+	                    List<IWM> resultList = new ArrayList<>();
+	                    for (int i = 0; i < resultsArray.length(); i++) {
+	                        JSONObject resultObject = resultsArray.getJSONObject(i);
+	                        IWM iwm = new IWM();
+	                        iwm.setVbeln_salesDocument(resultObject.getString("Vbeln_salesDocument"));
+	                        iwm.setCharg_batch(resultObject.getString("Charg_batch"));
+	                        iwm.setAbgru_rejectionReason(resultObject.getString("Abgru_rejectionReason"));
+	                        iwm.setKdmat_customerMaterial(resultObject.getString("Kdmat_customerMaterial"));
+	                        iwm.setWerks_plant(resultObject.getString("Werks_plant"));
+	                        iwm.setNet_wt_Manifestweight(resultObject.getString("Net_wt_Manifestweight"));
+	                        iwm.setVehicleno_vehicleNumber(resultObject.getString("Vehicleno_vehicleNumber"));
+	                        iwm.setNet_wt_VehicleWeight(resultObject.getString("Net_wt_VehicleWeight"));
+	                        iwm.setErdat_creationDate(DateParser.parseDateTime(resultObject.getString("erdat_creationDate")));
+	                        iwm.setAuart_SalesDocTy(resultObject.getString("Auart_SalesDocTy"));
+	                        if(!"00-00-0000".equals(resultObject.getString("aedat_changedDate"))) {
+		                        iwm.setAedat_changedDate(DateParser.parseDateTime(resultObject.getString("aedat_changedDate")));
+	                        }else {
+		                        iwm.setAedat_changedDate(null);
+	                        }
+	                        iwm.setPosnr_salesItem(resultObject.getString("Posnr_salesItem"));
+	                        iwm.setGbstk_overallStatus(resultObject.getString("Gbstk_overallStatus"));
+	                        iwm.setFaksk_billingBlock(resultObject.getString("Faksk_billingBlock"));
+	                        iwm.setStatusDescription(resultObject.getString("StatusDescription"));
+	                        iwm.setKunnr_customer(resultObject.getString("Kunnr_customer"));
+	                        iwm.setName1_name(resultObject.getString("Name1_name"));
+	                        iwm.setWaste_name(resultObject.getString("Waste_name"));
+	                        iwm.setManifest_no(resultObject.getString("Mainfest_No"));
+	                        iwm.setIwma_no(resultObject.getString("IWMA_NO"));
+	                        resultList.add(iwm);
+	                    }
+	                    if(resultList.size() > 0) {
+	                    	service.uploadIWM3Records(resultList);
+	                    }
+	                    // Print the list
+	                    System.out.println("Result List: " + resultList);
+	                } else {
+	                    System.out.println("Error: HTTP " + response.getStatusLine().getStatusCode());
+	                }
+	            	
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }//});
+		     }
+	}
+	
+	@Scheduled(cron = "${cron.expression.user.login.weight2}")
+	public void WEIGHT2() throws Exception{	
+		 IWM obj = null;
+		 HashMap<String, String> data1 = new HashMap<String, String>();
+		 ObjectMapper objectMapper = new ObjectMapper();
+ 		if(is_cron_jobs_enabled || is_cron_jobs_enabled_in_qa) {
+ 			//List<DashBoardWeighBridge> pData = service.projectsIWMList("IWM");
+ 		    LocalDate yesterday = LocalDate.now().minusDays(1);
+ 	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+ 	        System.out.println("wfw");
+ 	        String yesterdaysDate = yesterday.format(formatter);
+ 		//	pData.forEach(plist-> {
+ 	
+
+		     String url = "http://10.100.1.7:8000/sap/opu/odata/sap/Z_IDWM_WEIGHT_CDS/Z_IDWM_WEIGHT?$format=json&$filter=Werks_plant%20eq%20%273614%27%20and%20"
+		     		+ "CreationDate%20ge%20datetime%27"
+		     		+ yesterdaysDate
+		     		+ "T24:00:00%27%20";
+	            String username = "22011982";
+	            String password = "Ramky@567";
+
+	            CredentialsProvider provider = new BasicCredentialsProvider();
+	            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
+	            provider.setCredentials(AuthScope.ANY, credentials);
+
+	            try (CloseableHttpClient httpClient = HttpClients.custom()
+	                    .setDefaultCredentialsProvider(provider)
+	                    .build()) {
+	                HttpGet request = new HttpGet(url);
+
+	                HttpResponse response = httpClient.execute(request);
+
+	                if (response.getStatusLine().getStatusCode() == 200) {
+	                    HttpEntity entity = response.getEntity();
+	                    String responseBody = EntityUtils.toString(entity);
+	                    // Parse the JSON response
+	                    JSONObject jsonResponse = new JSONObject(responseBody);
+	                    JSONObject dObject = jsonResponse.getJSONObject("d");
+	                    JSONArray resultsArray = dObject.getJSONArray("results");
+
+
+	                    // Process the data and set it in a list
+	                    List<IWM> resultList = new ArrayList<>();
+	                    for (int i = 0; i < resultsArray.length(); i++) {
+	                        JSONObject resultObject = resultsArray.getJSONObject(i);
+	                        IWM iwm = new IWM();
+	                        iwm.setVbeln_salesDocument(resultObject.getString("Vbeln_salesDocument"));
+	                        iwm.setCharg_batch(resultObject.getString("Charg_batch"));
+	                        iwm.setAbgru_rejectionReason(resultObject.getString("Abgru_rejectionReason"));
+	                        iwm.setKdmat_customerMaterial(resultObject.getString("Kdmat_customerMaterial"));
+	                        iwm.setWerks_plant(resultObject.getString("Werks_plant"));
+	                        iwm.setNet_wt_Manifestweight(resultObject.getString("Net_wt_Manifestweight"));
+	                        iwm.setVehicleno_vehicleNumber(resultObject.getString("Vehicleno_vehicleNumber"));
+	                        iwm.setNet_wt_VehicleWeight(resultObject.getString("Net_wt_VehicleWeight"));
+	                        iwm.setErdat_creationDate(DateParser.parseDateTime(resultObject.getString("erdat_creationDate")));
+	                        iwm.setAuart_SalesDocTy(resultObject.getString("Auart_SalesDocTy"));
+	                        if(!"00-00-0000".equals(resultObject.getString("aedat_changedDate"))) {
+		                        iwm.setAedat_changedDate(DateParser.parseDateTime(resultObject.getString("aedat_changedDate")));
+	                        }else {
+		                        iwm.setAedat_changedDate(null);
+	                        }
+	                        iwm.setPosnr_salesItem(resultObject.getString("Posnr_salesItem"));
+	                        iwm.setGbstk_overallStatus(resultObject.getString("Gbstk_overallStatus"));
+	                        iwm.setFaksk_billingBlock(resultObject.getString("Faksk_billingBlock"));
+	                        iwm.setStatusDescription(resultObject.getString("StatusDescription"));
+	                        iwm.setKunnr_customer(resultObject.getString("Kunnr_customer"));
+	                        iwm.setName1_name(resultObject.getString("Name1_name"));
+	                        iwm.setWaste_name(resultObject.getString("Waste_name"));
+	                        iwm.setManifest_no(resultObject.getString("Mainfest_No"));
+	                        iwm.setIwma_no(resultObject.getString("IWMA_NO"));
+	                        resultList.add(iwm);
+	                    }
+	                    if(resultList.size() > 0) {
+	                    	service.uploadIWM3Records(resultList);
+	                    }
+	                    // Print the list
+	                    System.out.println("Result List: " + resultList);
+	                } else {
+	                    System.out.println("Error: HTTP " + response.getStatusLine().getStatusCode());
+	                }
+	            	
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }//});
+		     }
+	}
+	
+	@Scheduled(cron = "${cron.expression.user.login.weight1}")
+	public void WEIGHT3() throws Exception{	
+		 IWM obj = null;
+		 HashMap<String, String> data1 = new HashMap<String, String>();
+		 ObjectMapper objectMapper = new ObjectMapper();
+ 		if(is_cron_jobs_enabled || is_cron_jobs_enabled_in_qa) {
+ 		//	List<DashBoardWeighBridge> pData = service.projectsIWMList("IWM");
+ 		    LocalDate yesterday = LocalDate.now().minusDays(1);
+ 	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+ 	        System.out.println("wfw");
+ 	        String yesterdaysDate = yesterday.format(formatter);
+ 		//	pData.forEach(plist-> {
  				
  			
-		     String url = "http://10.100.1.7:8000/sap/opu/odata/sap/Z_IDWM_WEIGHT_CDS/Z_IDWM_WEIGHT?$format=json&"
-		    	 		+ "$filter=Werks_plant%20eq%20%273626%27%20or%20Werks_plant%20eq%20%273614%27%20or%20Werks_plant%20eq%20%273603%27%20"
+		     String url = "http://10.100.1.7:8000/sap/opu/odata/sap/Z_IDWM_WEIGHT_CDS/Z_IDWM_WEIGHT?$format=json&$filter=Werks_plant%20eq%20%273603%27%20and%20"
+		     		+ "CreationDate%20ge%20datetime%27"
+		     		+ yesterdaysDate
+		     		+ "T24:00:00%27%20"
+		    	 		//+ "and%20CreationDate%20ge%20datetime%272023-10-01T24:00:00%27%20and%20CreationDate%20le%20datetime%272023-10-31T24:00:00%27"
 		    		 
 		     		;
 	            String username = "22011982";
@@ -244,7 +420,7 @@ public class Schedular {
 	            	
 	            } catch (Exception e) {
 	                e.printStackTrace();
-	            }});
+	            }//});
 		     }
 	}
 	private String capitalize( String line) {
